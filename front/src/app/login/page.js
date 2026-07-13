69b5e5e4c6e9b19b87d0565d0943f489a0b91be5
Polyana Dias <polyana.rdsr@gmail.com>
2026-07-13 17:22:52 -0300

    Alteracoes nas telas

--- front/src/app/login/page.js
+++ front/src/app/login/page.js
@@ -1,27 +1,59 @@
 'use client';
+import { login } from "@/services/authService";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import  Button  from "@/components/Button";
 import FormInput from "@/components/FormInput";
 
 export default function LoginPage() {
   const [user, setUser] = useState({ email: "", password: "" });
-
+  const router = useRouter();
+  
   const handleChange = (e) => {
     const { name, value } = e.target;
     setUser((prevUser) => ({ ...prevUser, [name]: value }));
   };
 
-  const authenticate = () => {
-    // Lógica de autenticação aqui
-    console.log("Autenticando usuário:", user);
-  };
+  const authenticate = async () => {
+  if (!user.email || !user.password) {
+    alert("Preencha email e senha.");
+    return;
+  }
 
-    const loadBootstrap = () => {
-        fetch("http://localhost:8080/api/public/bootstrap")
-            .then((response) => response.json())
-            .then((data) => console.log("Bootstrap carregado:", data))
-            .catch((error) => console.error("Erro ao carregar Bootstrap:", error));
-    };
+  try {
+    const data = await login(user.email, user.password);
+
+    localStorage.setItem("token", data.accessToken);
+    localStorage.setItem("user", JSON.stringify(data.user));
+
+    alert("Login realizado com sucesso!");
+
+    router.push("/");
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+    const loadBootstrap = async () => {
+  try {
+    const response = await fetch(
+      "http://localhost:8080/api/public/bootstrap",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    console.log("Bootstrap carregado:", data);
+
+  } catch (error) {
+    console.error("Erro no bootstrap:", error);
+  }
+};
 
   return (
     <div>
